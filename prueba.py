855ab0ab70b28c993ea278438d3c6832bf3b61c5
Pablo2606 <martinalegrep@gmail.com>
2025-04-24 13:49:15 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,8 +1,4 @@
 def saludar():
     print("Hola mundo soy Marcos")
 
-<<<<<<< HEAD
-print ("Soy Pablo")
-=======
-print("Soy Marcos")
->>>>>>> 372512407df60e9e34daed3b85b0fa66be896a1b
+print("Somos Pablo y Marcos")
